1e62d0119e002b69e50d5b7e5696e15b4da64f8a
juhanncheong <hjim1188@gmail.com>
2025-06-03 19:17:26 -0400

    Fix User.js schema and remove duplicate exports

--- models/User.js
+++ models/User.js
@@ -1,20 +1,9 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email:    { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  createdAt:{ type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('User', UserSchema);
-
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
   balance: {
     type: Number,
     default: 0,
