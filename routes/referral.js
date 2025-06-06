c50a1bceff6567b92c31429af8f48f4989744b1e
juhanncheong <hjim1188@gmail.com>
2025-06-05 22:26:43 -0400

    Final fix: removed referral.js and route

--- routes/referral.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { generateReferralCode, lookupReferralCode } = require("../controller/adminController");
-
-router.get("/generate", generateReferralCode);
-router.get("/lookup", lookupReferralCode);
-
-module.exports = router;
