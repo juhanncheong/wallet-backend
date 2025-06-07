a2361693fabe4ad7ffacc1d2d4e146f6d80048bf
juhanncheong <hjim1188@gmail.com>
2025-06-07 16:47:47 -0400

    k

--- controller/adminController.js
+++ controller/adminController.js
@@ -296,7 +296,6 @@ const toggleWithdrawLock = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
-const User = require("../models/User");
 
 exports.getAllUsers = async (req, res) => {
   try {
